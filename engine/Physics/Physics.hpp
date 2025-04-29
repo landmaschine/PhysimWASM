2312e5db1a55812425e3b247b3f8e3402fa1e05d
landmaschine <leon@echoton.com>
2025-04-29 19:38:27 +0200

    update

---
+++ engine/Physics/Physics.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "core/common.hpp"
+
+class VerletSolver {
+public:
+
+private:
+
+};
+
+class LeapFrogSolver {
+public:
+
+private:
+
+};
+
+class PhysicsEngine {
+public:
+  PhysicsEngine();
+  ~PhysicsEngine();
+
+  
+
+private:
+
+};
