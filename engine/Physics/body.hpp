1da26a79568ef3e5d63650963ade6c8c7751836d
landmaschine <leon@echoton.com>
2025-04-30 21:32:58 +0200

    update

--- engine/Physics/body.hpp
+++ engine/Physics/body.hpp
@@ -2,4 +2,17 @@
 
 #include "core/common.hpp"
 
+struct RigidBody_t {
+  glm::vec2 position;
+  float rotation;
+  glm::vec2 velocity;
+  float angularVelocity;
+  float mass;
+  float invMass;
+  float inertia;
+  float invInertia;
+  glm::vec2 forceAccumulator;
+  float torqueAccumulator;
+} Rigidbody;
+
 
